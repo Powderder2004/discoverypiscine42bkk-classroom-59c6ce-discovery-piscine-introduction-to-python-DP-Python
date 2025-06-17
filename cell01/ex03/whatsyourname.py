07b08ba9d379ca1eb4c939688e1dc156667c2b3c
Powderder2004 <thanitapang2547@gmail.com>
2025-06-17 15:24:18 +0000

    whatsyourname

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,3 @@
-first_name = "Thanita"
-last_name = "Kruaisahwat"
-print(first_name, last_name)
+first_name = input("Hey, what's your first name? : ")
+last_name = input("And your last name? : ")
+print("Well, pleased to meet you,", first_name, last_name)
