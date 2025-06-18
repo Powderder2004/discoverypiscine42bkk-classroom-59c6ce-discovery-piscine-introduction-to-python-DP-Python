0f3051a70767f9ee3af03796ad780728f2bddd24
Powderder2004 <thanitapang2547@gmail.com>
2025-06-18 16:28:05 +0000

    help_your_professor.py 1

--- cell07/ex02/help_your_professor.py
+++ cell07/ex02/help_your_professor.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
-def find_the_redheads(family_dict):
-    return list(filter(lambda name: family_dict[name] == "red", family_dict))
+def average(student_scores):
+    total = sum(student_scores.values())
+    count = len(student_scores)
+    return total / count if count != 0 else 0
 
-dupont_family = {
-    "florian": "red",
-    "marie": "blond",
-    "virginie": "brunette",
-    "david": "red",
-    "franck": "red"
+class_3B = {
+    "marine": 18,
+    "jean": 15,
+    "coline": 8,
+    "luc": 9
 }
 
-print(find_the_redheads(dupont_family))
+class_3C = {
+    "quentin": 17,
+    "julie": 15,
+    "marc": 8,
+    "stephanie": 13
+}
+
+print(f"Average for class 3B: {average(class_3B)}.")
+print(f"Average for class 3C: {average(class_3C)}.")
